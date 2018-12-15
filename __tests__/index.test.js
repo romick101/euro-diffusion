4901010565117638ba924a2c9561a702d589c305
romick101 <romick101@gmail.com>
2018-12-15 21:52:55 +0200

    split file processing between separate functions, update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,16 @@
 describe('diffusion unit tests', () => {
-    it('returns correct result for dataset2', () => {
+    it('returns correct result for dataset1', () => {
         const getOutput = require('../src/index')
-        expect(getOutput('./data/dataset2')).toEqual([
+        expect(getOutput('./data/dataset1')).toEqual([
             {
                 Spain: 382,
                 Portugal: 416,
                 France: 1325 
+            }, {
+                Luxembourg: 0
+            }, {
+                Belgium: 2, 
+                Netherlands: 2
             }
         ])
     })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const map = require('./Map')
+const fs = require('fs')
 
 function compareChunkToString(chunk, string) {
     return Buffer.compare(chunk, Buffer.from(string)) === 0 ? true : false
 }
+
 function getCountryParams (stream) {
     const params = []
     while (null !== (chunk = stream.read(1))) {
@@ -17,32 +19,39 @@ function getCountryParams (stream) {
       }
     return params
 }
+
 function getCountryName (stream) {
     let name = ''
     while (null !== (chunk = stream.read(1))) {
+        if (compareChunkToString(chunk, '\r'))
+            continue
+        if (compareChunkToString(chunk, '\n'))
+            continue
         if (compareChunkToString(chunk, ' '))
             break
         name = name.concat(chunk.toString())
       }
     return name
 }
-function processFile (path) {
-    const fs = require('fs'),
-    readStream = fs.createReadStream(path);
-    readStream.on('readable', () => {
+
+function processSingleCase(stream, countriesNumber) {
+    for(i=0; i<countriesNumber; i++) {
+        const name = getCountryName(stream)
+        const params = getCountryParams(stream)
+        console.log(`got country ${name}:(${params})`)
+      }
+}
+function processStream (stream) {
+    stream.on('readable', () => {
         let chunk;
-        while (null !== (chunk = readStream.read(1))) {
-          console.log(`${chunk}`);
-          for(i=0; i<Number(chunk); i++) {
-            const name = getCountryName(readStream);
-            const params = getCountryParams(readStream);
-            console.log(`got country ${name}:(${params})`);
-          }
+        while (null !== (chunk = stream.read(1))) {
+          processSingleCase(stream, Number(chunk))
         }
-      });
+      })
 }
 function getOutput (inputPath) {
-    return processFile(inputPath)
+    const readStream = fs.createReadStream(inputPath)
+    return processStream(readStream)
 }
 
 module.exports = getOutput
